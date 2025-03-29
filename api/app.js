5966c140bfb04e61964408766ae556e4913c6114
Jesse Chan <z5477899@ad.unsw.edu.au>
2025-03-29 16:14:40 +1100

    l

--- api/app.js
+++ api/app.js
@@ -8,15 +8,10 @@ const port = 3000;
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, 'swagger.yaml'));
 
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve Swagger UI
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// Serve raw Swagger JSON (for debugging)
-app.get('/swagger.json', (req, res) => {
-  res.json(swaggerDocument);
-});
 
 // Basic route to check API is working
 app.get('/', (req, res) => {
