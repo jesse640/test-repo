c7f7659db6ea044cafae635cccecb385d789e435
Jesse Chan <z5477899@ad.unsw.edu.au>
2025-03-29 15:18:02 +1100

    swagger stuff

--- api/app.js
+++ api/app.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
+
+const swaggerDocument = YAML.load(path.join(__dirname, '../swagger.yaml'));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+
 app.get('/', (req, res) => {
   res.send('Hello World from Express!');
 });
