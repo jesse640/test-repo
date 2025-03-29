d322fb9d276a412cda328e7230e1e1c94dae266f
Jesse Chan <z5477899@ad.unsw.edu.au>
2025-03-29 15:59:42 +1100

    f

--- api/app.js
+++ api/app.js
@@ -1,34 +1,23 @@
 const express = require('express');
-const app = express();
-const port = 3000;
-
-const fs = require('fs');
-
-
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const path = require('path');
 
-// Read Swagger JSON
-const swaggerDocument = JSON.parse(fs.readFileSync(path.join(__dirname, 'public/swagger.json'), 'utf8'));
+const app = express();
+const swaggerDocument = YAML.load(path.join(__dirname, '../swagger.yaml'));
 
 // Serve Swagger UI
-app.use('/api-docs', swaggerUi.serve, (req, res, next) => {
-  req.swaggerDoc = swaggerDocument;
-  return swaggerUi.setup(swaggerDocument)(req, res, next);
-});
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// Serve raw JSON
+// Serve raw Swagger JSON (for debugging)
 app.get('/swagger.json', (req, res) => {
   res.json(swaggerDocument);
 });
 
+// Basic route to check API is working
 app.get('/', (req, res) => {
   res.send('Hello World from Express!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
 
 module.exports = app;
