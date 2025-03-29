953775ae42848720bba481bc5933adc9dca61635
Jesse Chan <z5477899@ad.unsw.edu.au>
2025-03-29 16:20:05 +1100

    l

--- api/app.js
+++ api/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const path = require('path');
+const serveStatic = require('serve-static'); // ✅ Ensure static files are served
+
 
 const port = 3000;
 
@@ -10,7 +12,9 @@ const swaggerDocument = YAML.load(path.join(__dirname, 'swagger.yaml'));
 
 
 // Serve Swagger UI
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', serveStatic(path.join(__dirname, 'node_modules/swagger-ui-dist')));
+
 
 
 // Basic route to check API is working
