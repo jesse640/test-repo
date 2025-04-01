e1e1ce39f71da6a23b1b19d71af663c183eccb12
Jesse Chan <z5477899@ad.unsw.edu.au>
2025-04-02 00:32:04 +1100

    c

--- api/app.js
+++ api/app.js
@@ -2,12 +2,26 @@ const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const path = require('path');
+const swaggerJsdoc = require('swagger-jsdoc');
 
 
 const port = 3000;
 
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '/public/swagger.yaml'));
+
+
+// const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
+
+// // Swagger setup
+// const swaggerSpec = swaggerJsdoc(swaggerDocument)
+
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+//     customCss:
+//         '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+//     customCssUrl: CSS_URL,
+// }
+// ))
 
 
 // Serve Swagger UI
