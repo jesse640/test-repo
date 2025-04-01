18c0a1ab9768e6e822d967c7667ad1be48d089e5
Jesse Chan <z5477899@ad.unsw.edu.au>
2025-04-02 10:22:43 +1100

    pls work

--- api/app.js
+++ api/app.js
@@ -8,25 +8,37 @@ const swaggerJsdoc = require('swagger-jsdoc');
 const port = 3000;
 
 const app = express();
-const swaggerDocument = YAML.load(path.join(__dirname, '/public/swagger.yaml'));
+// const swaggerDocument = YAML.load(path.join(__dirname, '/public/swagger.yaml'));
 
 
-// const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
 
-// // Swagger setup
-// const swaggerSpec = swaggerJsdoc(swaggerDocument)
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Hello World',
+      version: '1.0.0',
+    },
+  },
+  apis: [path.join(__dirname, 'public', 'swagger.yaml')], // Correct path
+};
 
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
-//     customCss:
-//         '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
-//     customCssUrl: CSS_URL,
-// }
-// ))
+
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
+
+// Swagger setup
+const swaggerSpec = swaggerJsdoc(options)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+    customCss:
+        '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+    customCssUrl: CSS_URL,
+}
+))
 
 
 // Serve Swagger UI
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 
 // Basic route to check API is working
