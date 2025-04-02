fbf74a9c45b450b0b95f097e3352860de880fbe9
Jesse Chan <z5477899@ad.unsw.edu.au>
2025-04-02 23:15:37 +1100

    fix

--- api/app.js
+++ api/app.js
@@ -1,44 +1,14 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
 const path = require('path');
-const swaggerJsdoc = require('swagger-jsdoc');
-
-
-const port = 3000;
+const YAML = require('yamljs')
 
 const app = express();
-// const swaggerDocument = YAML.load(path.join(__dirname, '/public/swagger.yaml'));
 
-
-
-const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Hello World',
-      version: '1.0.0',
-    },
-  },
-  apis: [path.join(__dirname, 'public', 'swagger.yaml')], // Correct path
-};
-
-
-const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
 
 // Swagger setup
-const swaggerSpec = swaggerJsdoc(options)
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
-    customCss:
-        '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
-    customCssUrl: CSS_URL,
-}
-))
-
-
-// Serve Swagger UI
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const swaggerDocument = YAML.load(path.join(__dirname, 'swagger.yaml'));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 
 // Basic route to check API is working
@@ -47,8 +17,8 @@ app.get('/', (req, res) => {
 });
 
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server is running on port 3000');
 });
 
 module.exports = app;
